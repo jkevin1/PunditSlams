aacc8edda446e04c76cbc109db65bfde04cb8a18
Kevin Jones <jkevin1@umbc.edu>
2020-07-30 23:49:20 -0400

    small improvements

--- PunditSlams.py
+++ PunditSlams.py
@@ -2,21 +2,23 @@ import tweepy
 import requests
 import json
 
+import api_keys
+
 url = ('http://newsapi.org/v2/everything?'
-       'q=slams&'
-       'from=2020-07-28&'
+       'qInTitle=Slams&'
+       'from=2020-07-15&'
        'sortBy=popularity&'
-       'apiKey=<key>')
+       'apiKey=' + api_keys.newsapi)
 
 response = requests.get(url)
 
 response_json = response.json()
 for article in response_json["articles"]: 
-    print(article["title"])
+    print(article["title"]) 
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("<key>", "<key>")
-auth.set_access_token("<key>", "<key>")
+auth = tweepy.OAuthHandler(api_keys.twitter_auth_a, api_keys.twitter_auth_b)
+auth.set_access_token(api_keys.twitter_access_a, api_keys.twitter_access_b)
 
 # Create API object
 api = tweepy.API(auth)
